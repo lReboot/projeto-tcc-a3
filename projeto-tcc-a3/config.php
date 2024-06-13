1894d3e1074eb39c9a99f47a7f06aa0fc4a84aee
Guilherme Lima <guilimakrv@gmail.com>
2024-06-12 23:47:30 -0300

    feat: data base

---
+++ projeto-tcc-a3/config.php
@@ -0,0 +1,17 @@
+<?php
+    $dbHost = 'LocalHost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'tcc-formulario';
+
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+    
+    if($conexao->connect_errno)
+    {
+        echo "Erro";
+    }
+    else
+    {
+        echo "Conexão efetuada com sucesso";
+    }
+?>
